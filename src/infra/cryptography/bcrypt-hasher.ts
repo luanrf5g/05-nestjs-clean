c46f2d3292a70f21650500c0ec31f7f1cce5318f
luanrf5g <of.luan.rferreira@gmail.com>
2024-10-17 20:37:37 -0300

    implements: refactoring authenticate controller

--- src/infra/cryptography/bcrypt-hasher.ts
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -1,7 +1,9 @@
 import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
 import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { Injectable } from '@nestjs/common'
 import { compare, hash } from 'bcryptjs'
 
+@Injectable()
 export class BcryptHaser implements HashGenerator, HashComparer {
   private HASH_SALT_LENGTH = 8
 
