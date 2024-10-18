1a6566754d26884a8c1f636ed72115b981452d5b
luanrf5g <of.luan.rferreira@gmail.com>
2024-10-18 18:51:04 -0300

    create: controller and tests for answer question

---
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,46 @@
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { userPayload } from '@/infra/auth/jwt.strategy'
+
+const answerQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+type AnswerQuestionBodySchema = z.infer<typeof answerQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(answerQuestionBodySchema)
+
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: AnswerQuestionBodySchema,
+    @CurrentUser() user: userPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const userId = user.sub
+
+    const result = await this.answerQuestion.execute({
+      authorId: userId,
+      questionId,
+      content,
+      attachmentsIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
